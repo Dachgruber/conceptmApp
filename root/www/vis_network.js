8a8d237ea8c557fa8d4113f99ed67774ab98a273
stu229317 <stu229317@mail.uni-kiel.de>
2022-12-10 14:40:44 +0100

    creating functions

---
+++ root/www/vis_network.js
@@ -0,0 +1,34 @@
+/**
+ * adds a node to the network
+ * @param {name} String - The name for the node
+ */
+function AddNode(name){
+       
+}
+
+/**
+ * deletes a node from the network
+ * @param {name} String - The name of the node
+ * @param {nodeID} int - ID of the node to be deleted
+ */
+function DeleteNode(name, nodeID){
+   
+}
+
+/**
+ * adds an edge to the network
+ * @param {name} String - The name for the edge
+ */
+  function AddEdge(name){
+     
+}
+
+/**
+ * deletes an edge from the network
+ * @param {name} String - The name of the edge
+ * @param {edgeID} int - ID of the edge to be deleted
+ */
+  function DeleteEdge(name, edgeID){
+   
+} 
+
